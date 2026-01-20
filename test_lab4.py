504b6f952fab5cb728fb94599c5718ad972ef07a
sebastianleenerves27 <sebastianleenerves@DESKTOP-BKV0EL9.localdomain>
2026-01-20 23:44:50 +0800

    add test_lab4.py for lab4

---
+++ test_lab4.py
@@ -0,0 +1,31 @@
+
+
+
+import unittest
+
+from lab4_pattern import Logger
+
+class TestMath(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(2 + 3, 5)
+
+    def test_subtract(self):
+        self.assertEqual(10 - 4, 6)
+
+    def test_multiply(self):
+        self.assertEqual(3 * 4, 12)
+
+    def test_divide(self):
+        self.assertEqual(12 / 3, 4)
+
+class TestLoggerSingleton(unittest.TestCase):
+
+    def test_singleton_instance(self):
+        logger1 = Logger()
+        logger2 = Logger()
+        self.assertIs(logger1, logger2)
+
+if __name__ == "__main__":
+    unittest.main()
+
